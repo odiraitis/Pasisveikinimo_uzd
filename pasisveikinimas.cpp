1243d67c3521de641a023a06aca93a6b702c6003
Oskaras <oskarasdiraitis@gmail.com>
2019-02-07 10:40:04 +0200

    Ikeliu pasisveikinimo uzduoti

---
+++ pasisveikinimas.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+int main()
+{
+    std::string vardas;
+    int remelis=3;
+    std::cout<<"Iveskite savo varda"<<std::endl;
+    std::cin>>vardas;
+    int kiek=0;
+    for(int i=0; i<vardas[i]; i++)
+    {
+        kiek++;
+    }
+   if(vardas[kiek-1]=='s')
+   {
+       for(int i=0;i<kiek+2+10+2; i++) std::cout<<"*";
+       std::cout<<std::endl;
+       for(int i=0; i<remelis; i++)
+       {std::cout<<"*";
+           for(int j=0; j<kiek+12; j++)
+           {
+               std::cout<<" ";
+           }
+           std::cout<<"*";
+           std::cout<<std::endl;
+       }
+       std::cout<<"* ";
+       std::cout<<"Sveikas, "<<vardas<<"! *"<<std::endl;
+       for(int i=0; i<remelis; i++)
+       {std::cout<<"*";
+           for(int j=0; j<kiek+12; j++)
+           {
+               std::cout<<" ";
+           }
+           std::cout<<"*";
+           std::cout<<std::endl;
+       }
+         for(int i=0;i<kiek+2+10+2; i++) std::cout<<"*";
+       
+    
+   }
+    else
+    {
+        for(int i=0;i<kiek+2+10+1; i++) std::cout<<"*";
+        std::cout<<std::endl;
+        for(int i=0; i<remelis; i++)
+        {std::cout<<"*";
+            for(int j=0; j<kiek+11; j++)
+            {
+                std::cout<<" ";
+            }
+            std::cout<<"*";
+            std::cout<<std::endl;
+        }
+        std::cout<<"* ";
+        std::cout<<"Sveika, "<<vardas<<"! *"<<std::endl;
+        for(int i=0; i<remelis; i++)
+        {std::cout<<"*";
+            for(int j=0; j<kiek+11; j++)
+            {
+                std::cout<<" ";
+            }
+            std::cout<<"*";
+            std::cout<<std::endl;
+        }
+        for(int i=0;i<kiek+2+10+1; i++) std::cout<<"*";
+        
+
+    }
+
+}
